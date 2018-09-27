4809ae6f579a7103fcc3ab8a8c10feafbd4bcb26
coin2016 <coin2016@mail.com>
2018-09-27 15:48:09 +0000

    main node address

--- src/net.cpp
+++ src/net.cpp
@@ -1195,12 +1195,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"5.196.67.100", "5.196.67.100"},
+    {"95.179.183.193", "95.179.183.193"},
     {NULL, NULL} 
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-   // {"MMXVI.org", "dnsseed.MMXVI.org"},
+    {"95.179.183.193", "95.179.183.193"},
     {NULL, NULL}
 };
 
